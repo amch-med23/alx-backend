f7b92aadcc27ee53bace979c53dfccec73c3cb70
amch-med23 <skyied.med2023@gmail.com>
2024-05-16 12:19:56 +0000

    fixing a typo in 1-app.py and rewrite some parameters

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config(object):
+class Config:
     """ The config class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
