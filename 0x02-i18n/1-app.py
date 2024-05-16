157ea17600a818b43ca238ba6bd94a81ce860cda
amch-med23 <skyied.med2023@gmail.com>
2024-05-16 12:00:41 +0000

    adding the script 1-app.py that has a support for flask_babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" A flask template that supports babel"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config(object):
+    """ The config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFUALT_TIMEZONE = 'UTC'
+
+
+# flask app configurations
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ the index function """
+    return render_template('1-index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
